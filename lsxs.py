39a3847ebed0878f4bfa1fd9ab896d5fc0bccb42
marcell marosvolgyi <marcell@noisegate.org>
2016-04-27 16:08:01 +0200

    Update lsxs.py

--- lsxs.py
+++ lsxs.py
@@ -130,13 +130,13 @@ class Lsxs(object):
 
     def leftvertical(self):
         self.ul()
-        #self.command = "G91"
-        self.command = "G1 X10 Y20"
+        self.command = "G91"
+        self.command = "G1 X0 Y10"
         #self.pulse()
         self.command = "G90"
         self.ll()
-        #self.command = "G91"
-        self.command = "G1 X10 Y490"
+        self.command = "G91"
+        self.command = "G1 X0 Y-10"
         #self.pulse()
     
     def lowerhorizontal(self):
